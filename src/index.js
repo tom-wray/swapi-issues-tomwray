c7c5c9eb7a5fa334b8d92b89178c3c74a7dcb521
Tom Wray <60654606+tom-wray@users.noreply.github.com>
2025-02-20 19:37:51 +0000

    Added create jira ticket function to resolver

--- src/index.js
+++ src/index.js
@@ -2,20 +2,35 @@ import Resolver from '@forge/resolver';
 import api, { route } from '@forge/api';
 const resolver = new Resolver();
 
-resolver.define('fetchLabels', async (req) => {
-  const key = req.context.extension.issue.key;
+resolver.define("createJiraTicket", async (req) => {
+  const projectKey = req.context.extension.project.key;
 
-  const res = await api.asUser().requestJira(route`/rest/api/3/issue/${key}?fields=labels`);
+  const res = await api.asUser().requestJira(route`/rest/api/3/issue`, {
+      method: "POST",
+      headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+          fields: {
+              project: {
+                  key: projectKey,
+              },
+              summary: `${req.payload.characterSummary}`,
+              issuetype: {
+                  name: "Task",
+              },
+          },
+      }),
+  });
 
-  const data = await res.json();
-
-  const label = data.fields.labels;
-  if (label == undefined) {
-    console.warn(`${key}: Failed to find labels`);
-    return [];
+  if (!res.ok) {
+      const errorDetails = await res.json();
+      console.error(errorDetails);
+      throw new Error(`HTTP error! status: ${res.status}`);
   }
 
-  return label;
+  return res.json();
 });
 
 export const handler = resolver.getDefinitions();
